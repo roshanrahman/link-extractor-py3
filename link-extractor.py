1f9986909dce30c90f7f75860c172828f2e1e6ae
Roshan <roshanrahman@outlook.com>
2018-06-01 18:07:42 +0530

    Modified the script to be more structured

--- link-extractor.py
+++ link-extractor.py
@@ -14,14 +14,20 @@ link= ""
 link_list = []
 web_text = ""
 write_file_name = "" 
+PRINT_ENABLED = True
+
+def println(str):
+    if(PRINT_ENABLED):
+        print(str)
+    return
 
 def get_write_file_name():
     file_name = input("Enter the file name where you want to store the fetched links: ").strip()
     if(len(file_name) == 0):
-        print("You did not specify the file name. The results will be stored in link.txt")
+        println("You did not specify the file name. The results will be stored in link.txt")
         return "link.txt"
     if("." not in file_name):
-        print("You didn't specify the file extension. It will be saved as a txt file.")
+        println("You didn't specify the file extension. It will be saved as a txt file.")
         file_name = file_name + ".txt"
     return file_name
     
@@ -39,31 +45,31 @@ else:
 extension = input("Enter the file extension the links must end with (ex. pdf, doc, mp3 etc): ")
 extension = extension.replace(".", "")
 if(len(extension) == 0):
-    print("No extension specified. All links on the webpage will be fetched.")
+    println("No extension specified. All links on the webpage will be fetched.")
 regex_string = "." + extension + "$"
 
-print("Fetching the webpage...")
+println("Fetching the webpage...")
 try:
-    web_text = urllib.request.urlopen("https://github.com/EbookFoundation/free-programming-books/blob/master/free-programming-books.md")
+    web_text = urllib.request.urlopen(link)
 except:
-    print("Could not fetch the webpage.")
+    println("Could not fetch the webpage.")
     exit(0)
 
-print("Parsing using BeautifulSoup...")
+println("Parsing using BeautifulSoup...")
 try:
     web_obj = BeautifulSoup(web_text, "html5lib")
     link_list = web_obj.findAll('a', attrs={'href': re.compile(regex_string)})
 except:
-    print("Could not parse the webpage.")
+    println("Could not parse the webpage.")
     exit(0)
     
 if(len(link_list) == 0):
-    print("No links were found.")
+    println("No links were found.")
 else:
-    print("No. of links found: " + str(len(link_list)))
+    println("No. of links found: " + str(len(link_list)))
     sys.stdout = open(write_file_name, "a+")
     for link in link_list:
-        print(link.get('href'))
+        println(link.get('href'))
     sys.stdout = sys.__stdout__
-    print("Links written to file " + write_file_name)
+    println("Links written to file " + write_file_name)
 exit(0)
