cdaa71ee24647243a0ffe753c8e509bfe0626cfe
Roshan <roshanrahman@outlook.com>
2018-06-01 18:16:18 +0530

    Changed variable names

--- link-extractor.py
+++ link-extractor.py
@@ -10,10 +10,10 @@ import urllib
 import re
 from bs4 import BeautifulSoup
 
-link= "" 
-link_list = []
-web_text = ""
-write_file_name = "" 
+LINK_URL= "" 
+RESULTS = []
+URL_SOUP = ""
+WRITE_FILE_NAME = ""
 PRINT_ENABLED = True
 
 def println(str):
@@ -21,55 +21,55 @@ def println(str):
         print(str)
     return
 
-def get_write_file_name():
-    file_name = input("Enter the file name where you want to store the fetched links: ").strip()
+def get_WRITE_FILE_NAME():
+    file_name = input("Enter the file name where you want to store the fetched LINK_URLs: ").strip()
     if(len(file_name) == 0):
-        println("You did not specify the file name. The results will be stored in link.txt")
-        return "link.txt"
+        println("You did not specify the file name. The results will be stored in LINK_URL.txt")
+        return "LINK_URL.txt"
     if("." not in file_name):
         println("You didn't specify the file extension. It will be saved as a txt file.")
         file_name = file_name + ".txt"
     return file_name
     
 if(len(sys.argv) == 1):
-    link = input("Enter a valid URL: ")
-    write_file_name = get_write_file_name()
+    LINK_URL = input("Enter a valid URL: ")
+    WRITE_FILE_NAME = get_WRITE_FILE_NAME()
 else:
-    link = sys.argv[1]
+    LINK_URL = sys.argv[1]
     if(len(sys.argv) == 3):
-        write_file_name = sys.argv[2]
+        WRITE_FILE_NAME = sys.argv[2]
     else:
-        write_file_name = get_write_file_name()
+        WRITE_FILE_NAME = get_WRITE_FILE_NAME()
     
 
-extension = input("Enter the file extension the links must end with (ex. pdf, doc, mp3 etc): ")
+extension = input("Enter the file extension the LINK_URLs must end with (ex. pdf, doc, mp3 etc): ")
 extension = extension.replace(".", "")
 if(len(extension) == 0):
-    println("No extension specified. All links on the webpage will be fetched.")
+    println("No extension specified. All LINK_URLs on the webpage will be fetched.")
 regex_string = "." + extension + "$"
 
 println("Fetching the webpage...")
 try:
-    web_text = urllib.request.urlopen(link)
+    URL_SOUP = urllib.request.urlopen(LINK_URL)
 except:
     println("Could not fetch the webpage.")
     exit(0)
 
 println("Parsing using BeautifulSoup...")
 try:
-    web_obj = BeautifulSoup(web_text, "html5lib")
-    link_list = web_obj.findAll('a', attrs={'href': re.compile(regex_string)})
+    web_obj = BeautifulSoup(URL_SOUP, "html5lib")
+    RESULTS = web_obj.findAll('a', attrs={'href': re.compile(regex_string)})
 except:
     println("Could not parse the webpage.")
     exit(0)
     
-if(len(link_list) == 0):
-    println("No links were found.")
+if(len(RESULTS) == 0):
+    println("No LINK_URLs were found.")
 else:
-    println("No. of links found: " + str(len(link_list)))
-    sys.stdout = open(write_file_name, "a+")
-    for link in link_list:
-        println(link.get('href'))
+    println("No. of LINK_URLs found: " + str(len(RESULTS)))
+    sys.stdout = open(WRITE_FILE_NAME, "a+")
+    for LINK_URL in RESULTS:
+        println(LINK_URL.get('href'))
     sys.stdout = sys.__stdout__
-    println("Links written to file " + write_file_name)
+    println("LINK_URLs written to file " + WRITE_FILE_NAME)
 exit(0)
