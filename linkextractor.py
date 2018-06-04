2e0803ffd5717674619b757e105156f44b477988
Roshan <roshanrahman@outlook.com>
2018-06-04 16:41:42 +0530

    Added docstrings

--- linkextractor.py
+++ linkextractor.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun May 13 16:26:52 2018
+Link extractor
+
+Simple script that can fetch links that end with a specific extension, useful to fetch links to files such as mp3, pdf, etc.
 
 @author: Roshan
 """
@@ -21,6 +23,12 @@ def println(str):
 
 
 def get_file_name():
+    '''Gets a file name from the user. Requires text file format. If extension is not specified, 
+    it will use txt instead. If nothing is specified, file 'results.txt' will be used instead.
+    
+    RETURNS: A file name with extension, of type str
+    '''
+
     file_name = input(
         "Enter the file name where you want to store the fetched urls: ").strip()
     if(len(file_name) == 0):
@@ -35,6 +43,11 @@ def get_file_name():
 
 
 def get_url():
+    '''Gets a valid URL from the user. Requires URL to begin with http or https. If the URL entered is invalid,
+    the function recursively attempts to get a valid URL.
+    
+    RETURNS: A regex-matched URL, of type str
+    '''
     url = input("Enter a valid URL: ")
     regex = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
     if(re.search(regex, url)):
@@ -45,6 +58,11 @@ def get_url():
 
 
 def get_extension():
+    '''Gets a valid extension from the user. If the extension is not specified, it returns an empty string.
+    Removes all dots the user may enter.
+    
+    RETURNS: An extension with all dots removed, of type str.
+    '''
     extension = input(
         "Enter the file EXTENSION the URLs must end with (ex. pdf, doc, mp3 etc) or ignore: ")
     extension = extension.replace(".", "")
@@ -54,7 +72,12 @@ def get_extension():
 
 
 def get_all_links_by_soup(url, extension):
-
+    '''Fetches all links from the webpage located by the URL, that end with the extension specified.
+    Uses BeautifulSoup to retrieve all href links from the <a> tags in the source code. If no links
+    are fetched, the returned list is empty.
+    
+    RETURNS: A list containing the fetched links.
+    '''
     results = []
     regex_string = '(http[s]?:\/\/)?[^\s(["<,>]*\.[^\s[",><]*' + \
         '.' + extension + '$'
@@ -72,7 +95,12 @@ def get_all_links_by_soup(url, extension):
 
 
 def get_all_links_by_regex(url, extension):
-
+    '''Fetches all links from the webpage located by the URL, that end with the extension specified.
+    Uses Regex to retrieve all the matching links in the source code. If no links
+    are fetched, the returned list is empty.
+    
+    RETURNS: A list containing the fetched links.
+    '''
     results = []
     regex_string = '(http[s]?:\/\/?[^\s(["<,>]*\.[^\s[",><]*' + \
         '.' + extension + ')'
@@ -87,6 +115,9 @@ def get_all_links_by_regex(url, extension):
 
 
 def write_results_to_file(results, filename):
+    '''Writes the results to file specified, if the result list is not empty.
+    If the file already exists, the data will be appended to the end of file, otherwise new file will be created.
+    '''
     if(len(results) == 0):
         println("No URLs were found.")
     else:
@@ -96,11 +127,13 @@ def write_results_to_file(results, filename):
         sys.stdout = sys.__stdout__
         println("Results written to file " + filename)
 
+
 if __name__ == "__main__":
 
     PRINT_ENABLED = True
     try:
-        choice = int(input("Select option: \n1. Get all <a> links (using BeautifulSoup)\n2. Capture every URL in webpage text (using Regex)\n> "))
+        choice = int(input(
+            "Select option: \n1. Get all <a> links (using BeautifulSoup)\n2. Capture every URL in webpage text (using Regex)\n> "))
     except:
         println("Incorrect option.\n")
         exit(0)
